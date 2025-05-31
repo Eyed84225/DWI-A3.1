3bfb4bf068803533ddd5b12b74bd21e7243c5e30
Eyed84225 <tmiller84225@uat.edu>
2025-05-31 14:28:00 -0700

    Add files via upload

---
+++ Design int 1.js
@@ -0,0 +1,3 @@
+document.getElementById("Name").textContent = "Taren Miller";
+document.getElementById("Major").textContent = "Major: Advancing Computer Science";
+document.getElementById("GradDate").textContent = "I am expected to graduate around May 2027.";
